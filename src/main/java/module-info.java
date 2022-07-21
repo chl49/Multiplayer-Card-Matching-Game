9bab3cee50cbfc5a5f6d1f3d0a43c6755d2268e2
siddhant <sbpatel@sfu.ca>
2022-07-21 14:07:43 -0700

    start here

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.cmpt371project {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.cmpt371project to javafx.fxml;
+    exports com.example.cmpt371project;
+}
