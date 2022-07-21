25ed7e4d7dde842f07fd9afed9c8cc9d759daa19
chl49 <chl49@sfu.ca>
2022-07-21 16:20:00 -0700

    changed HelloApplication and nulled Controller

--- src/main/java/com/example/cmpt371project/HelloController.java
+++ src/main/java/com/example/cmpt371project/HelloController.java
@@ -16,29 +16,6 @@ public class HelloController {
     private Stage stage;
     private Scene scene;
     private Parent root;
-    public void switchToHomeScene2(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void switchToServerScene2(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("ServerScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void switchToClientScene2(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("ClientScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
     @FXML
     private Label welcomeText;
 
