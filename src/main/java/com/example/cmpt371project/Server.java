65d1c9bffe233f274aa983afe75192625f9c0629
chl49 <chl49@sfu.ca>
2022-08-04 16:08:27 -0700

    server with unimplemented TODO stuff

--- src/main/java/com/example/cmpt371project/Server.java
+++ src/main/java/com/example/cmpt371project/Server.java
@@ -13,13 +13,29 @@ public class Server implements Runnable{
     //Convert to thread eventually
     //player array variable to store players ports (only used for sending game board at the start)
     private boolean hasMessage;
-    private static PriorityQueue<String> messages = new PriorityQueue<>();
+    private static PriorityQueue<String[]> messages = new PriorityQueue<>();
     //
     private final int MAX_PLAYERS = 1;
     private final DatagramPacket[] playerData = new DatagramPacket[MAX_PLAYERS];
     private int currentPlayers = 0;
     private boolean serverRunning = false;
     private boolean gameNotFinished = false;
+
+    private synchronized void updateMessageQueue(String[] msg){
+        while (hasMessage) {
+            // no room for new message
+            try {
+                wait();  // release the lock of this object
+            } catch (InterruptedException ignored) { }
+        }
+        // acquire the lock and continue
+        hasMessage = true;
+        System.out.println("updating message queue");
+        //add packet to messages queue with priority on timestamp
+        messages.add(msg);
+        // notify processing Priority queue thread to take over
+        notify();
+    }
 
     public void run() {
         try {
@@ -89,18 +105,30 @@ public class Server implements Runnable{
 
                 //Herb: TODO:::: Server Multicast
                 String[] data = IncomingData.split(",");
-                if (data[0].equals("Button")) {
+                if (data[0].equals("Clicked")) {
                     //NEW ###Herb: Acknowledge users connected, locking clients to port
                     String Message = "Connected";
-                    byte[] buffer_out = Message.getBytes();
+
+                    //Herb: TODO:::: Queue processing
+                    updateMessageQueue(data);
                     byte[] buffer_cast = Message.getBytes();
                     //Send Data to other players
                     for (int i = 0; i < MAX_PLAYERS; i++){
                         InetAddress Add = playerData[i].getAddress();
                         int P = playerData[i].getPort();
-                        DatagramPacket packet_cast = new DatagramPacket(buffer_cast, buffer_cast.length, Add, P);
-                        System.out.println("Sending to " + Add + " on port: " + P);
-                        socket.send(packet_cast); // send data
+                        //Herb: TODO:::: Player unique stuff
+                        //EX. data = [Clicked,0,1] == Action, Grid, Player
+                        if(Integer.parseInt(data[2])==i){
+                            DatagramPacket packet_cast = new DatagramPacket(buffer_cast, buffer_cast.length, Add, P);
+                            System.out.println("Sending to " + Add + " on port: " + P);
+                            socket.send(packet_cast); // send data
+
+                        }
+                        else{
+                            DatagramPacket packet_cast = new DatagramPacket(buffer_cast, buffer_cast.length, Add, P);
+                            System.out.println("Sending to " + Add + " on port: " + P);
+                            socket.send(packet_cast); // send data
+                        }
                     }
                 }
             }
